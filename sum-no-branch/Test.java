37251e49b9669cf2aba0dc6b330c6b7513346c96
Galder Zamarreño <galder@zamarreno.com>
2024-11-07 12:39:15 +0100

    Add test

---
+++ sum-no-branch/Test.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+class Test
+{
+    static final int SIZE = 16 * 1024;
+    static final int THRESHOLD = 4096;
+
+    static int test(int[] array)
+    {
+        int sum = 0;
+
+        for (final int value : array)
+        {
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] array = new int[SIZE];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.nextInt(THRESHOLD);
+        }
+
+        final int result = test(array);
+        blackhole(result);
+
+    }
+
+    static void blackhole(int value)
+    {
+        if ((long) value == System.nanoTime())
+        {
+            System.out.println(obj);
+        }
+    }
+}
