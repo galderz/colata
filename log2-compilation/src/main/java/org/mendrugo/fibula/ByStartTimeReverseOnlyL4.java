023d190f023a53717fac602660321a21f8d25127
Galder Zamarreño <galder@zamarreno.com>
2024-11-13 10:59:49 +0100

    Decompose compilation print into its print calls

--- log2-compilation/src/main/java/org/mendrugo/fibula/ByStartTimeReverseOnlyL4.java
+++ log2-compilation/src/main/java/org/mendrugo/fibula/ByStartTimeReverseOnlyL4.java
@@ -3,13 +3,17 @@ package org.mendrugo.fibula;
 import com.sun.hotspot.tools.compiler.Compilation;
 import com.sun.hotspot.tools.compiler.LogEvent;
 import com.sun.hotspot.tools.compiler.LogParser;
+import com.sun.hotspot.tools.compiler.Method;
 import com.sun.hotspot.tools.compiler.NMethod;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class ByStartTimeReverseOnlyL4
 {
+    public static final long JVM_ACC_SYNCHRONIZED = 0x0020; /* wrap method call in monitor lock */
+
     /**
      * Sort log events by start time, reversed.
      */
@@ -63,17 +67,55 @@ public class ByStartTimeReverseOnlyL4
             }
             if (c instanceof Compilation comp)
             {
-                if (comp.getNMethod() != null &&
-                    4 == comp.getNMethod().getLevel()
+                final NMethod nMethod = comp.getNMethod();
+                if (nMethod != null &&
+                    4 == nMethod.getLevel()
                 )
                 {
-                    comp.print(System.out, printID, printInlining);
+                    // Destructure the call below into its components
+                    // comp.print(System.out, printID, printInlining);
+
+                    System.out.print(comp.getId());
+                    System.out.print(" " + nMethod.getLevel());
+
+                    final long nMethodSize = nMethod.getInstSize();
+
+                    String codeSize = "";
+                    if (nMethodSize > 0)
+                    {
+                        codeSize = "(code size: " + nMethodSize + ")";
+                    }
+
+                    final int bc = comp.isOsr() ? comp.getBCI() : -1;
+                    System.out.print(decodeFlags(bc, comp) + " " + comp.getCompiler() + " " + format(bc, comp.getMethod()) + codeSize);
+                    System.out.println();
                 }
             }
-            else
-            {
-                // c.print(System.out, printID);
-            }
+//            else
+//            {
+//                c.print(System.out, printID);
+//            }
         }
+    }
+
+    static String decodeFlags(int osr_bci, Compilation comp)
+    {
+        int f = Integer.parseInt(comp.getMethod().getFlags());
+        char[] c = new char[4];
+        Arrays.fill(c, ' ');
+        if (osr_bci >= 0)
+        {
+            c[0] = '%';
+        }
+        if ((f & JVM_ACC_SYNCHRONIZED) != 0)
+        {
+            c[1] = 's';
+        }
+        return new String(c);
+    }
+
+    static String format(int osr_bci, Method method)
+    {
+        return osr_bci >= 0 ? method.getHolder() + "::" + method.getName() + " @ " + osr_bci + " (" + method.getBytes() + " bytes)" : method.getHolder() + "::" + method.getName() + " (" + method.getBytes() + " bytes)";
     }
 }
