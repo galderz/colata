17ab79f3d5e00eeac9e18704eb5135a1320312a8
Galder Zamarreño <galder@zamarreno.com>
2025-02-13 10:49:32 +0100

    Add more options and floatToIntBits example

---
+++ fp-bits-vector-8329077/FloatToIntBits.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FloatToIntBits
+{
+    static void test(int[] ints, float[] floats)
+    {
+        for (int i = 0; i < ints.length; i++)
+        {
+            ints[i] = Float.floatToIntBits(floats[i]);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        final int[] ints = new int[10_000];
+        final float[] floats = new float[10_000];
+
+        for (int i = 0; i < 100_000; i++)
+        {
+            test(ints, floats);
+        }
+    }
+}
