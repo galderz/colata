18354e6ebf07ee66fe1b8b81ea48dce1a98138d6
Galder Zamarreño <galder@zamarreno.com>
2025-02-14 05:58:08 +0100

    Switch test to flowToRawIntBits

--- fp-bits-vector-8329077/Test.java
+++ fp-bits-vector-8329077/Test.java
@@ -1,7 +1,32 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Test
 {
+    static void test(int[] ints, float[] floats)
+    {
+        for (int i = 0; i < ints.length; i++)
+        {
+            ints[i] = Float.floatToRawIntBits(floats[i]);
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        final int[] ints = new int[10_000];
+        final float[] floats = new float[10_000];
+        // init(ints);
+
+        for (int i = 0; i < 100_000; i++)
+        {
+            test(ints, floats);
+        }
     }
+
+//    static void init(int[] ints) {
+//        final ThreadLocalRandom rand = ThreadLocalRandom.current();
+//        for (int i = 0; i < ints.length; i++)
+//        {
+//            ints[i] = rand.nextInt();
+//        }
+//    }
 }
