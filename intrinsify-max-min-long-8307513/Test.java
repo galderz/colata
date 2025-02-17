1c3a37cfd1005c4de3698c71b582d87b0986aedf
Galder Zamarreño <galder@zamarreno.com>
2025-02-17 17:14:56 +0100

    Add a test for int max

--- intrinsify-max-min-long-8307513/Test.java
+++ intrinsify-max-min-long-8307513/Test.java
@@ -1,51 +1,108 @@
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 class Test
 {
-    static final int RANGE = 16*1024;
+    static final int RANGE = 16 * 1024;
     static final int ITER = 100_000;
-
-    static long init(long[] a, long[] b, long[] c) {
-        ThreadLocalRandom rand = ThreadLocalRandom.current();
-        for (int j = 0; j < RANGE; j++)
-        {
-            a[j] = rand.nextLong();
-            b[j] = rand.nextLong();
-            c[j] = rand.nextLong();
-        }
-        return rand.nextLong();
-    }
-
-    static long test(long[] a, long[] b, long[] c, long total)
-    {
-        for (int i = 0; i < RANGE; i++)
-        {
-            long v = (a[i] * b[i]) + (a[i] * c[i]) + (b[i] * c[i]);
-            total = Math.max(total, v);
-        }
-        return total;
-    }
 
     public static void main(String[] args)
     {
-        long[] a = new long[RANGE];
-        long[] b = new long[RANGE];
-        long[] c = new long[RANGE];
-        long start = init(a, b, c);
-        long gold = test(a, b, c, start);
-        for (int i = 0; i < ITER; i++)
+        final int probability = Integer.parseInt(args[0]);
+
+        final DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator(' ');
+        final DecimalFormat format = new DecimalFormat("#,###", symbols);
+
+        System.out.printf("Probability: %d%%%n", probability);
+        int[] a = new int[64 * 1024];
+        init(a, probability);
+
         {
-            long total = test(a, b, c, start);
-            verify("long max", total, gold);
+            System.out.println("Warmup");
+            for (int i = 0; i < 10_000; i++)
+            {
+                test1(a);
+            }
+            System.out.println("Run");
+            long t0 = System.nanoTime();
+            for (int i = 0; i < 10_000; i++)
+            {
+                test1(a);
+            }
+            long t1 = System.nanoTime();
+            System.out.println("Time: " + format.format(t1 - t0));
+        }
+
+        {
+            System.out.println("Warmup");
+            for (int i = 0; i < 10_000; i++)
+            {
+                test2(a);
+            }
+            System.out.println("Run");
+            long t0 = System.nanoTime();
+            for (int i = 0; i < 10_000; i++)
+            {
+                test2(a);
+            }
+            long t1 = System.nanoTime();
+            System.out.println("Time: " + format.format(t1 - t0));
         }
     }
 
-    static void verify(String context, long total, long gold)
+    public static int test1(int[] a)
     {
-        if (total != gold)
+        int x = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++)
         {
-            throw new RuntimeException("Wrong result for " + context + ": " + total + " != " + gold);
+            x = Math.max(x, a[i]);
         }
+        return x;
+    }
+
+    public static int test2(int[] a)
+    {
+        int x = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++)
+        {
+            x = (x >= a[i]) ? x : a[i];
+        }
+        return x;
+    }
+
+    public static void init(int[] ints, int probability)
+    {
+        int aboveCount, abovePercent;
+
+        do
+        {
+            int max = ThreadLocalRandom.current().nextInt(10);
+            ints[0] = max;
+
+            aboveCount = 0;
+            for (int i = 1; i < ints.length; i++)
+            {
+                int value;
+                if (ThreadLocalRandom.current().nextInt(101) <= probability)
+                {
+                    int increment = ThreadLocalRandom.current().nextInt(10);
+                    value = max + increment;
+                    aboveCount++;
+                }
+                else
+                {
+                    // Decrement by at least 1
+                    int decrement = ThreadLocalRandom.current().nextInt(10) + 1;
+                    value = max - decrement;
+                }
+                ints[i] = value;
+                max = Math.max(max, value);
+            }
+
+            abovePercent = ((aboveCount + 1) * 100) / ints.length;
+        } while (abovePercent != probability);
     }
 }
