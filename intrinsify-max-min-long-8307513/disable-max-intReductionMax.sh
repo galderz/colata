c6214e30d8dae573dea3ecb3e1b0be16f68699f8
Galder Zamarreño <galder@zamarreno.com>
2025-02-18 06:14:56 +0100

    Add disable int reduction max tests

--- intrinsify-max-min-long-8307513/disable-max-intReductionMax.sh
+++ intrinsify-max-min-long-8307513/disable-max-intReductionMax.sh
@@ -7,5 +7,11 @@ set -e -x
 # ...
 # 5536  487 %     4       org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionMax_jmhTest::intReductionMax_thrpt_jmhStub @ 13 (59 bytes)   made not entrant
 
-CONF=release TEST="micro:lang.MinMaxVector.intReductionMax" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:CompileCommand=option,org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionMax_jmhTest::intReductionMax_thrpt_jmhStub,ccstrlist,DisableIntrinsic,_max -prof perfasm" make micro
-CONF=release TEST="micro:lang.MinMaxVector.intReductionMax" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:CompileCommand=option,org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionMax_jmhTest::intReductionMax_thrpt_jmhStub,ccstrlist,DisableIntrinsic,_max" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax" MICRO="FORK=1" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax" MICRO="FORK=1;OPTIONS=-prof perfasm" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:CompileCommand=option,org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionSimpleMax_jmhTest::intReductionSimpleMax_thrpt_jmhStub,ccstrlist,DisableIntrinsic,_max" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:CompileCommand=option,org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionSimpleMax_jmhTest::intReductionSimpleMax_thrpt_jmhStub,ccstrlist,DisableIntrinsic,_max -prof perfasm" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax" MICRO="FORK=1" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-prof perfasm" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:CompileCommand=option,org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionMultiplyMax_jmhTest::intReductionMultiplyMax_thrpt_jmhStub,ccstrlist,DisableIntrinsic,_max" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:CompileCommand=option,org.openjdk.bench.java.lang.jmh_generated.MinMaxVector_intReductionMultiplyMax_jmhTest::intReductionMultiplyMax_thrpt_jmhStub,ccstrlist,DisableIntrinsic,_max -prof perfasm" make micro
