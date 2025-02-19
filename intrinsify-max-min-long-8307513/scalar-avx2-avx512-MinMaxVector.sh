b00f3c100fd9149138bdf316b7ee56c32c3ba300
Galder Zamarreño <galder@zamarreno.com>
2025-02-19 10:39:41 +0100

    Add test for scalar behaviour on AVX2 and AVX-512

---
+++ intrinsify-max-min-long-8307513/scalar-avx2-avx512-MinMaxVector.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord -prof perfasm" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord -prof perfasm" make micro
