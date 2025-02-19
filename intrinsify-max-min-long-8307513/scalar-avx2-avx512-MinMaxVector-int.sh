23cc9607cc723e16bc0c42145487c661b7c8e8b4
Galder Zamarreño <galder@zamarreno.com>
2025-02-19 18:42:43 +0100

    Add scalar comparison for MinMaxVector.int

---
+++ intrinsify-max-min-long-8307513/scalar-avx2-avx512-MinMaxVector-int.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
