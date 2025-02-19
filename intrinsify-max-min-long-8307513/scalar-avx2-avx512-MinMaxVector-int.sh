81b050608098298ebd555af03068e34e5db87797
Galder Zamarreño <galder@zamarreno.com>
2025-02-19 18:48:13 +0100

    Disable min/max int intrinsics

--- intrinsify-max-min-long-8307513/scalar-avx2-avx512-MinMaxVector-int.sh
+++ intrinsify-max-min-long-8307513/scalar-avx2-avx512-MinMaxVector-int.sh
@@ -2,10 +2,10 @@
 
 set -e -x
 
-TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
-TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
-TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
-TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max,_min -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max,_min -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max,_min -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max,_min -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
 
 TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
 TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.int" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
