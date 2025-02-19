a5e865b642677767d9b264b9cb530ad4099216ab
Galder Zamarreño <galder@zamarreno.com>
2025-02-19 11:14:10 +0100

    Run disabling maxL intrinsic to compare

--- intrinsify-max-min-long-8307513/scalar-avx2-avx512-MinMaxVector.sh
+++ intrinsify-max-min-long-8307513/scalar-avx2-avx512-MinMaxVector.sh
@@ -2,6 +2,11 @@
 
 set -e -x
 
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord -prof perfasm" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" make micro
+CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord -prof perfasm" make micro
+
 CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord" make micro
 CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:-UseSuperWord -prof perfasm" make micro
 CONF=release TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.long" MICRO="FORK=1;OPTIONS=-jvmArgs -XX:UseAVX=2 -jvmArgs -XX:-UseSuperWord" make micro
