a2eefc087f1c72ebf56f8f31aa5f2ca4fd7528a0
Galder Zamarreño <galder@zamarreno.com>
2025-02-20 09:15:43 +0100

    Fix disable intrinsics for long and shape it for easier analysis

--- intrinsify-max-min-long-8307513/scalar-MinMaxVector-reduction-100.sh
+++ intrinsify-max-min-long-8307513/scalar-MinMaxVector-reduction-100.sh
@@ -2,27 +2,40 @@
 
 set -e -x
 
-benchmark()
-{
-    local profiler_opts=$1
+# Production run
+# First run all with intrinsics disabled, then enabled
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMin"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMin" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_minL -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMin"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMax"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMin"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_minL -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMin"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMin" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMin"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMax"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMin"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord" CONF=release BUILD_LOG=warn make micro
 
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMin"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMin"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMin" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMin" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMin"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMin"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMax"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMax"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMin"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-    TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMin"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord $profiler_opts" CONF=release BUILD_LOG=warn make micro
-}
-
-benchmark ""
-benchmark "-prof perfasm"
+# Profiling run
+# Alternatve intrinsic enable/disabled for easier capturing
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMax"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMin"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionMultiplyMin"  MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMax" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMin" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_minL -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionMultiplyMin" MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_max -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMax"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMin"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_min -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.intReductionSimpleMin"    MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMax"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMax"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMin"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_minL -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
+TEST="micro:org.openjdk.bench.java.lang.MinMaxVector.longReductionSimpleMin"   MICRO="FORK=1;OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -prof perfasm" CONF=release BUILD_LOG=warn make micro
