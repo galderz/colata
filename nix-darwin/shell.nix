e9e107407a2874795158f96de449b2467b79599f
Galder Zamarreño <galder@zamarreno.com>
2025-03-05 14:36:40 +0100

    Install clang and set MIGCC to its cc

--- nix-darwin/shell.nix
+++ nix-darwin/shell.nix
@@ -3,11 +3,11 @@
 let
   devkit = "/nix/store/vhsix1jn849mpxggwbw2zh1nbxpy0grc-Xcode16.2-MacOSX15";
 in
-#pkgs.mkShellNoCC {
 pkgs.mkShell {
   packages = [
     pkgs.autoconf
     pkgs.jdk23
+    pkgs.clang
 
     devkit
   ];
@@ -15,5 +15,8 @@ pkgs.mkShell {
   shellHook = ''
     echo "Setting DEVKIT_ROOT to path of the devkit in the Nix store."
     export DEVKIT_ROOT=${devkit}
+
+    echo "Setting MIGCC to clang compiler cc binary."
+    export MIGCC="${pkgs.clang}/bin/cc"
   '' ;
 }
