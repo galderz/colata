9a228af5b37c90bfdeb6aa2c6b312f8538d12689
Galder Zamarreño <galder@zamarreno.com>
2025-03-18 08:45:33 +0100

    First jdk work moved over to Nix setup

---
+++ avoid-cmov-long-min-max-8351409/README.md
@@ -0,0 +1,21 @@
+# Useful commands
+
+Interacting with this project is done withing a Nix shell.
+So all commands assume you're inside it:
+
+```shell
+> nix-shell
+[nix-shell:...]$
+```
+
+# Create worktree
+
+```shell
+$ make new-worktree
+```
+
+# Build the full JDK
+
+```shell
+$ make configure build-jdk
+```
---
+++ avoid-cmov-long-min-max-8351409/shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  devkit = "/nix/store/vhsix1jn849mpxggwbw2zh1nbxpy0grc-Xcode16.2-MacOSX15";
+in
+pkgs.mkShell {
+  packages = [
+    pkgs.autoconf
+    pkgs.capstone
+    pkgs.temurin-bin-23
+
+    devkit
+  ];
+
+  shellHook = ''
+    echo "Setting DEVKIT_ROOT to path of the devkit in the Nix store."
+    export DEVKIT_ROOT=${devkit}
+
+    echo "Setting MIGCC to clang compiler cc binary."
+    export MIGCC="${devkit}/Xcode/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc"
+
+    echo "Setting BOOT_JDK_HOME"
+    export BOOT_JDK_HOME="${pkgs.temurin-bin-23}"
+
+    echo "Setting CAPSTONE_HOME"
+    export CAPSTONE_HOME="${pkgs.capstone}"
+  '' ;
+}
