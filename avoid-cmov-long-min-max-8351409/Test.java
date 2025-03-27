2bcc003b7c2466a1f78739ae07336c9cb6fe385a
Galder Zamarreño <galder@zamarreno.com>
2025-03-27 17:32:25 +0100

    Validate max at each step

--- avoid-cmov-long-min-max-8351409/Test.java
+++ avoid-cmov-long-min-max-8351409/Test.java
@@ -7,12 +7,13 @@ public class Test
 
     // Initializes data with increasing values,
     // so max one of the branches is the one that is always taken
-    static void init(long[] data)
+    static long init(long[] data)
     {
         for (int i = 0; i < RANGE; i++)
         {
             data[i] = i + 1;
         }
+        return maxOf(data);
     }
 
     static long test(long[] data)
@@ -27,25 +28,41 @@ public class Test
         return max;
     }
 
+    static long maxOf(long[] data)
+    {
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < RANGE; i++)
+        {
+            final long value = 11 * data[i];
+            final long tmp = (max >= value) ? max : value;
+            max = tmp;
+        }
+        return max;
+    }
+
+    static void validate(long expected, long actual)
+    {
+        if (expected == actual)
+        {
+            System.out.print(".");
+        }
+        else
+        {
+            throw new AssertionError("Failed, expected: " + expected + ", actual: " + actual);
+        }
+    }
+
     public static void main(String[] args)
     {
         long[] data = new long[RANGE];
-        init(data);
+        long expectedMax = init(data);
+        System.out.println("Expected max: " + expectedMax);
 
         long result = 0;
         for (long i = 0; i < ITER; i++)
         {
             result = test(data);
-        }
-
-        // Validate
-        if (result == 11 * Arrays.stream(data).max().getAsLong())
-        {
-            System.out.println("Success");
-        }
-        else
-        {
-            throw new AssertionError("Failed, max: " + result);
+            validate(expectedMax, result);
         }
     }
 }
