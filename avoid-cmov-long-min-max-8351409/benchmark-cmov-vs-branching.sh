9e6988e95d41aed622945f5ba132fa6df4341a9c
Galder Zamarreño <galder@zamarreno.com>
2025-04-01 10:58:26 +0200

    Benchmark different branching/cmov options

--- avoid-cmov-long-min-max-8351409/benchmark-cmov-vs-branching.sh
+++ avoid-cmov-long-min-max-8351409/benchmark-cmov-vs-branching.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-set -eux
+set -ex
+
+CLEAN="true"
+
+# Check for clean parameter
+if [[ "$1" == "--clean=false" ]]; then
+    read -p "Are you sure you want to run without cleaning first? (yes/no): " RESPONSE
+    if [[ "$RESPONSE" == "yes" ]]; then
+        CLEAN="false"
+    else
+        echo "Exiting because you don't want to apply the changes."
+        exit 1
+    fi
+fi
 
 benchmark_all()
 {
@@ -14,7 +27,6 @@ benchmark_all()
     local class="micro:org.openjdk.bench.java.lang.MinMaxVector"
     local micro_args="OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord"
 
-    CONF=release BUILD_LOG=warn make clean-jdk
 
     log TEST=\"${class}.longReductionMultiplyMax\" MICRO=\"${micro_args} ${extra_args}\" CONF=release BUILD_LOG=warn make test
     log TEST=\"${class}.longReductionSimpleMax\" MICRO=\"${micro_args} ${extra_args}\" CONF=release BUILD_LOG=warn make test
@@ -26,14 +38,20 @@ log()
     eval "$*"
 }
 
+if [[ $CLEAN == "true" ]]; then
+  CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+fi
+
 benchmark_all "topic.avoid-cmov-long-min-max.0327.cmov" ""
 benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-always" ""
 benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-never" ""
 
 if [ "$(uname)" = "Darwin" ]; then
   PROFILER="xctraceasm"
+  SDKROOT=/Applications/Xcode.app/Contents/Developer
 else
   PROFILER="perfasm"
+  SDKROOT=$SDKROOT
 fi
 
 benchmark_all "topic.avoid-cmov-long-min-max.0327.cmov" "-prof $PROFILER;FORK=1"
