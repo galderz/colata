c9843c39f4bdda1fd8192a6eae58a52da8eaaed6
Galder Zamarreño <galder@zamarreno.com>
2025-04-02 09:33:35 +0200

    Benchmark fixing LoopMaxUnroll=8

---
+++ avoid-cmov-long-min-max-8351409/benchmark-loop-max-unroll.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CLEAN="true"
+
+if [ "$(uname)" = "Darwin" ]; then
+  PROFILER="xctraceasm"
+  SDKROOT=/Applications/Xcode.app/Contents/Developer
+else
+  PROFILER="perfasm"
+  SDKROOT=$SDKROOT
+fi
+
+# Check for clean parameter
+if [[ "$1" == "--clean=false" ]]; then
+    read -p "Are you sure you want to run without cleaning first? (yes/no): " RESPONSE
+    if [[ "$RESPONSE" == "yes" ]]; then
+        CLEAN="false"
+    else
+        echo "Exiting because you don't want to apply the changes."
+        exit 1
+    fi
+fi
+
+benchmark_all()
+{
+    local branch=$1
+    local extra_args=$2
+
+    pushd $HOME/1/jdk-avoid-cmov-long-min-max
+    git checkout ${branch}
+    popd
+
+    local prefix="micro:org\.openjdk\.bench\.java\.lang\.MinMaxVector\.longReduction"
+    local micro_args="OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord -jvmArgs -XX:LoopMaxUnroll=8"
+
+    log TEST=\"${prefix}\\\(?:Simple\\\|Multiply\\\)Max\" MICRO=\"${micro_args} ${extra_args}\" CONF=release BUILD_LOG=warn make test
+}
+
+log()
+{
+    echo "$*"
+    eval "$*"
+}
+
+if [[ $CLEAN == "true" ]]; then
+  CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+fi
+
+# Note: DisableIntrinsic requires UnlockDiagnosticVMOptions
+benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-never" "-prof $PROFILER;FORK=1"
+benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-never" ""
+benchmark_all "topic.avoid-cmov-long-min-max.base" "-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -prof $PROFILER;FORK=1"
+benchmark_all "topic.avoid-cmov-long-min-max.base" "-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL"
