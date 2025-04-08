b43eb0c27e63fc2ecdceb99249ff2ee51d2c6767
Galder Zamarreño <galder@zamarreno.com>
2025-04-08 11:16:42 +0200

    Use a separately downloaded boot jdk until 24 is in Nix

--- avoid-cmov-long-min-max-8351409/shell.nix
+++ avoid-cmov-long-min-max-8351409/shell.nix
@@ -18,7 +18,8 @@ pkgs.mkShell {
     export ANT_HOME="${pkgs.ant}/share/ant"
     echo "Setting ANT_HOME to $ANT_HOME"
 
-    export BOOT_JDK_HOME="${pkgs.temurin-bin-23}"
+    # export BOOT_JDK_HOME="${pkgs.temurin-bin-23}"
+    export BOOT_JDK_HOME="$HOME/opt/boot-java-24"
     echo "Setting BOOT_JDK_HOME to $BOOT_JDK_HOME"
 
     export CAPSTONE_HOME="${pkgs.capstone}"
