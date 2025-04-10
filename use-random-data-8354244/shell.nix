ba92b510c6fe549d34a9f5caaaadc9890195e6c6
Galder Zamarreño <galder@zamarreno.com>
2025-04-10 12:55:42 +0200

    Base setup for project

---
+++ use-random-data-8354244/shell.nix
@@ -0,0 +1,40 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  devkit = "/nix/store/ddwccf951n51ng61zql8y6wcvs65vzli-Xcode16.3-MacOSX15";
+in
+pkgs.mkShell {
+  packages = [
+    pkgs.ant
+    pkgs.autoconf
+    pkgs.capstone
+    pkgs.pigz
+    # pkgs.temurin-bin-23
+
+    devkit
+  ];
+
+  shellHook = ''
+    export ANT_HOME="${pkgs.ant}/share/ant"
+    echo "Setting ANT_HOME to $ANT_HOME"
+
+    # export BOOT_JDK_HOME="${pkgs.temurin-bin-23}"
+    export BOOT_JDK_HOME="$HOME/opt/boot-java-24"
+    echo "Setting BOOT_JDK_HOME to $BOOT_JDK_HOME"
+
+    export CAPSTONE_HOME="${pkgs.capstone}"
+    echo "Setting CAPSTONE_HOME to $CAPSTONE_HOME"
+
+    export DEVKIT_ROOT=${devkit}
+    echo "Setting DEVKIT_ROOT to $DEVKIT_ROOT"
+
+    export DEVELOPER_DIR="${devkit}/Xcode/Contents/Developer"
+    echo "Setting DEVELOPER_DIR to $DEVELOPER_DIR"
+
+    export MIGCC="$SDKROOT/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc"
+    echo "Setting MIGCC to $MIGCC"
+
+    unset SOURCE_DATE_EPOCH
+    echo "Unsetting SOURCE_DATE_EPOCH to avoid errors running tests"
+  '' ;
+}
