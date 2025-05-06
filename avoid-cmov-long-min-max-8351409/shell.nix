b111ae94b9869c210d2e9724522d8145d84f474f
Galder Zamarreño <galder@zamarreno.com>
2025-05-06 10:34:59 +0200

    Spell out requirements

--- avoid-cmov-long-min-max-8351409/shell.nix
+++ avoid-cmov-long-min-max-8351409/shell.nix
@@ -14,6 +14,11 @@ pkgs.mkShell {
     devkit
   ];
 
+  # Requirements:
+  # DEVKIT_ROOT set so that JDK build configure picks it up
+  # DEVELOPER_DIR set so that tools such as xctrace are resolved
+  # MIGCC overriden because xcrun cannot still locate it even though DEVELOPER_DIR is set:
+  #   xcrun: error: unable to find Xcode installation from active developer path "/nix/store/ddwccf951n51ng61zql8y6wcvs65vzli-Xcode16.3-MacOSX15/Xcode/Contents/Developer", use xcode-select to change
   shellHook = ''
     export ANT_HOME="${pkgs.ant}/share/ant"
     echo "Setting ANT_HOME to $ANT_HOME"
@@ -25,13 +30,13 @@ pkgs.mkShell {
     export CAPSTONE_HOME="${pkgs.capstone}"
     echo "Setting CAPSTONE_HOME to $CAPSTONE_HOME"
 
-    export DEVKIT_ROOT=${devkit}
+    export DEVKIT_ROOT="${devkit}"
     echo "Setting DEVKIT_ROOT to $DEVKIT_ROOT"
 
     export DEVELOPER_DIR="${devkit}/Xcode/Contents/Developer"
     echo "Setting DEVELOPER_DIR to $DEVELOPER_DIR"
 
-    export MIGCC="$SDKROOT/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc"
+    export MIGCC="$DEVELOPER_DIR/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc"
     echo "Setting MIGCC to $MIGCC"
 
     unset SOURCE_DATE_EPOCH
