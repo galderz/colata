ae0198a749ea642295641da23c848198a81ba889
Galder Zamarreño <galder@zamarreno.com>
2025-05-20 09:15:38 +0200

    Update for latest changes

--- avoid-cmov-long-min-max-8351409/benchmark-cmov-vs-branching.sh
+++ avoid-cmov-long-min-max-8351409/benchmark-cmov-vs-branching.sh
@@ -5,10 +5,10 @@ set -ex
 CLEAN="true"
 
 if [ "$(uname)" = "Darwin" ]; then
-  PROFILER="xctraceasm"
+  ASM_PROFILER="xctraceasm"
   DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
 else
-  PROFILER="perfasm"
+  ASM_PROFILER="perfasm"
 fi
 
 # Check for clean parameter
@@ -27,14 +27,33 @@ benchmark_all()
     local branch=$1
     local extra_args=$2
 
-    pushd $HOME/1/jdk-avoid-cmov-long-min-max
+    local prefix="micro:org\.openjdk\.bench\.java\.lang\.MinMaxVector\.longReduction"
+    local micro_args="OPTIONS=-jvmArgs -XX:-UseSuperWord"
+
+    log TEST=\"${prefix}\\\(?:Simple\\\|Multiply\\\)Max\" MICRO=\"${micro_args} ${extra_args}\" CONF=release BUILD_LOG=warn make test
+}
+
+benchmark_branch()
+{
+    local branch=$1
+    local extra_args=$2
+
+    pushd $HOME/src/jdk-avoid-cmov-long-min-max
     git checkout ${branch}
     popd
 
-    local prefix="micro:org\.openjdk\.bench\.java\.lang\.MinMaxVector\.longReduction"
-    local micro_args="OPTIONS=-p probability=100 -jvmArgs -XX:-UseSuperWord"
+    # Tracking regression needs to
+    if [[ $CLEAN == "true" ]]; then
+        CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+    fi
 
-    log TEST=\"${prefix}\\\(?:Simple\\\|Multiply\\\)Max\" MICRO=\"${micro_args} ${extra_args}\" CONF=release BUILD_LOG=warn make test
+#    for probability in \
+#      50 \
+#      80 \
+#      100
+#    do
+    benchmark_all ${branch} "${extra_args} -prof $ASM_PROFILER;FORK=1"
+#    done
 }
 
 log()
@@ -49,12 +68,6 @@ fi
 
 # DisableIntrinsic requires UnlockDiagnosticVMOptions
 
-benchmark_all "topic.avoid-cmov-long-min-max.0327.cmov" ""
-benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-always" ""
-benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-never" ""
-benchmark_all "topic.avoid-cmov-long-min-max.base" "-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL"
-
-benchmark_all "topic.avoid-cmov-long-min-max.0327.cmov" "-prof $PROFILER;FORK=1"
-benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-always" "-prof $PROFILER;FORK=1"
-benchmark_all "topic.avoid-cmov-long-min-max.0327.branch-never" "-prof $PROFILER;FORK=1"
-benchmark_all "topic.avoid-cmov-long-min-max.base" "-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL -prof $PROFILER;FORK=1"
+benchmark_branch "topic.avoid-cmov-long-min-max.0408.base" ""
+# benchmark_branch "topic.avoid-cmov-long-min-max.0408.base" "-jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:DisableIntrinsic=_maxL"
+benchmark_branch "topic.avoid-cmov-long-min-max.0408.branch-never-ge" ""
