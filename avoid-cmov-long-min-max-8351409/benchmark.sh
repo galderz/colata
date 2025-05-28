f8395e4f2426a5aa85e0ffd0a83110f4d4981c71
Galder Zamarreño <galder@zamarreno.com>
2025-05-28 19:29:45 +0200

    Benchmark all of MinMaxVector.long

---
+++ avoid-cmov-long-min-max-8351409/benchmark.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CLEAN="true"
+
+if [ "$(uname)" = "Darwin" ]; then
+  ASM_PROFILER="xctraceasm"
+  DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
+else
+  ASM_PROFILER="perfasm"
+fi
+
+# Check for clean parameter
+if [[ "$1" == "--clean=false" ]]; then
+    read -p "Are you sure you want to run without cleaning first? (yes/no): " RESPONSE
+    if [[ "$RESPONSE" == "yes" ]]; then
+        CLEAN="false"
+    else
+        echo "Exiting because you don't want to apply the changes."
+        exit 1
+    fi
+fi
+
+benchmark_all()
+{
+    local branch=$1
+    local extra_args=$2
+
+    local prefix="micro:org\.openjdk\.bench\.java\.lang\.MinMaxVector\.long"
+    local micro_args="OPTIONS=-jvmArgs -XX:-UseSuperWord"
+
+    log TEST=\"${prefix}\" MICRO=\"${micro_args} ${extra_args}\" CONF=release LOG=warn make test
+}
+
+benchmark_branch()
+{
+    local branch=$1
+    local extra_args=$2
+
+    pushd $HOME/src/jdk-avoid-cmov-long-min-max
+    git checkout ${branch}
+    popd
+
+    # Tracking regression needs to
+    if [[ $CLEAN == "true" ]]; then
+        CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+    fi
+
+    if [[ $branch != *base ]]; then
+        # Branch always max: -XX:-UseNewCode
+        # Branch always min: -XX:-UseNewCode2
+        # Branch never max:  -XX:+UseNewCode
+        # Branch never min:  -XX:+UseNewCode2
+        benchmark_all ${branch} "${extra_args} -p includeEquals=true -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:-UseNewCode -jvmArgs -XX:-UseNewCode2 -prof $ASM_PROFILER;FORK=1"
+        benchmark_all ${branch} "${extra_args} -p includeEquals=true -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:+UseNewCode -jvmArgs -XX:+UseNewCode2 -prof $ASM_PROFILER;FORK=1"
+    else
+        benchmark_all ${branch} "${extra_args} -p includeEquals=true -prof $ASM_PROFILER;FORK=1"
+    fi
+}
+
+log()
+{
+    echo "$*"
+    eval "$*"
+}
+
+if [[ $CLEAN == "true" ]]; then
+  CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+fi
+
+# DisableIntrinsic requires UnlockDiagnosticVMOptions
+# UseNewCode / UseNewCode requires UnlockDiagnosticVMOptions
+
+benchmark_branch "topic.avoid-cmov.0521.aarch64-x64" ""
+benchmark_branch "topic.avoid-cmov.0521.base" ""
