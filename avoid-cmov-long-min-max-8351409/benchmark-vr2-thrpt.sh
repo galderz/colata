1b00541357aef983a1846da3883317af1a2ea378
Galder Zamarreño <galder@zamarreno.com>
2025-06-02 08:02:18 +0200

    Set thrpt benchmark mode for VectorReduction2

---
+++ avoid-cmov-long-min-max-8351409/benchmark-vr2-thrpt.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CLEAN="true"
+
+if [ "$(uname)" = "Darwin" ]; then
+  ASM_PROFILER="xctraceasm"
+  DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
+else
+  ASM_PROFILER="perfasm"
+fi
+
+# Check for clean parameter
+if [[ "$1" == "--clean=false" ]]; then
+    read -p "Are you sure you want to run without cleaning first? (yes/no): " RESPONSE
+    if [[ "$RESPONSE" == "yes" ]]; then
+        CLEAN="false"
+    else
+        echo "Exiting because you don't want to apply the changes."
+        exit 1
+    fi
+fi
+
+benchmark_all()
+{
+    local branch=$1
+    local extra_args=$2
+    local rffPrefix=$3
+
+    log TEST=\"micro:org\.openjdk\.bench\.vm\.compiler\.VectorReduction2\.NoSuperword\.long\\\(?:Min\\\|Max\\\)\" MICRO=\"OPTIONS=-rff ${rffPrefix}-vr2.csv ${extra_args}\" CONF=release LOG=warn make test
+    # log TEST=\"micro:org\.openjdk\.bench\.java\.lang\.MinMaxVector\.long\" MICRO=\"OPTIONS=-jvmArgs -XX:-UseSuperWord -rff ${rffPrefix}-mmv.csv ${extra_args}\" CONF=release LOG=warn make test
+}
+
+benchmark_branch()
+{
+    local branch=$1
+    local extra_args=$2
+    local common_args="-p includeEquals=true -bm thrpt"
+
+    pushd $HOME/src/jdk-avoid-cmov-long-min-max
+    git checkout ${branch}
+    popd
+
+    # Tracking regression needs to
+    if [[ $CLEAN == "true" ]]; then
+        CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+    fi
+
+    if [[ $branch != *base ]]; then
+        # Branch always max: -XX:-UseNewCode
+        # Branch always min: -XX:-UseNewCode2
+        # Branch never max:  -XX:+UseNewCode
+        # Branch never min:  -XX:+UseNewCode2
+        benchmark_all ${branch} "${extra_args} ${common_args} -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:-UseNewCode -jvmArgs -XX:-UseNewCode2 -prof $ASM_PROFILER;FORK=1" "branch-always"
+        benchmark_all ${branch} "${extra_args} ${common_args} -jvmArgs -XX:+UnlockDiagnosticVMOptions -jvmArgs -XX:+UseNewCode -jvmArgs -XX:+UseNewCode2 -prof $ASM_PROFILER;FORK=1" "branch-never"
+    else
+        benchmark_all ${branch} "${extra_args} ${common_args} -prof $ASM_PROFILER;FORK=1" "base"
+    fi
+}
+
+log()
+{
+    echo "$*"
+    eval "$*"
+}
+
+if [[ $CLEAN == "true" ]]; then
+  CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+fi
+
+# DisableIntrinsic requires UnlockDiagnosticVMOptions
+# UseNewCode / UseNewCode requires UnlockDiagnosticVMOptions
+
+benchmark_branch "topic.avoid-cmov.0521.aarch64-x64" ""
+benchmark_branch "topic.avoid-cmov.0521.base" ""
