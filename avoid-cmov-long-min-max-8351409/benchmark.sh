e78524f6bff7717ee5ff5d7f294f9f60d32c7f52
Galder Zamarreño <galder@zamarreno.com>
2025-06-20 15:39:01 +0200

    Do not clean by default

--- avoid-cmov-long-min-max-8351409/benchmark.sh
+++ avoid-cmov-long-min-max-8351409/benchmark.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-CLEAN="true"
+CLEAN="false"
 EVENTS="cycles,instructions,branch-misses,br_mis_pred,inst_retired"
 
 if [ "$(uname)" = "Darwin" ]; then
@@ -13,10 +13,10 @@ else
 fi
 
 # Check for clean parameter
-if [[ "$1" == "--clean=false" ]]; then
-    read -p "Are you sure you want to run without cleaning first? (yes/no): " RESPONSE
+if [[ "$1" == "--clean=true" ]]; then
+    read -p "Are you sure you want to clean first? (yes/no): " RESPONSE
     if [[ "$RESPONSE" == "yes" ]]; then
-        CLEAN="false"
+        CLEAN="true"
     else
         echo "Exiting because you don't want to apply the changes."
         exit 1
