06368c690fdda4b404c02e3bc1e401fd40eacdaa
Galder Zamarreño <galder@ibm.com>
2025-07-22 10:14:52 +0100

    Remove unnecessary tests

--- fp-bits-vector-8329077/ci.sh
+++ fp-bits-vector-8329077/ci.sh
@@ -2,6 +2,4 @@
 
 set -e -x
 
-#TEST="test/hotspot/jtreg/compiler/jvmci/jdk.vm.ci.runtime.test/src/jdk/vm/ci/runtime/test/ConstantPoolTest.java" make test
-#TEST="test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java" make test
-TEST="test/hotspot/jtreg/compiler/c2/irTests/TestVectorFPConversion.java" make test
+TEST="test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java" make test
