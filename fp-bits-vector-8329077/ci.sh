210df5aebf2d502cd15e7fb1ec4b4331f5633b08
Galder Zamarreño <galder@ibm.com>
2025-08-11 14:59:18 +0200

    Add half point test

--- fp-bits-vector-8329077/ci.sh
+++ fp-bits-vector-8329077/ci.sh
@@ -3,3 +3,4 @@
 set -e -x
 
 TEST="test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java" make test
+TEST="test/hotspot/jtreg/compiler/c2/irTests/ConvF2HFIdealizationTests.java" make test
