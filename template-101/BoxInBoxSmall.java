0f5a97209e4f03198005f8393a683d349cf9c899
Galder Zamarreño <galder@ibm.com>
2025-08-12 15:17:57 +0200

    Show removed allocation nodes

--- template-101/BoxInBoxSmall.java
+++ template-101/BoxInBoxSmall.java
@@ -59,24 +59,14 @@ public class BoxInBoxSmall
     }
 }
 /**
+ * AFTER: BEFORE_ITER_GVN
+ *   27  Allocate  === 5 6 7 8 1 (25 23 24 1 1 _ _ _ 21 22 1 1 ) [[ 28 29 30 37 38 39 ]]  rawptr:NotNull ( int:>=0, java/lang/Object:NotNull *, bool, top, bool, bottom, java/lang/Object:NotNull *, long ) allocationKlass:BoxInBoxSmall$BoxSmall BoxInBoxSmall::test @ bci:7 (line 7) !jvms: BoxInBoxSmall::test @ bci:7 (line 7)
+ *  126  Allocate  === 114 38 115 8 1 (124 123 24 1 1 _ _ _ 1 1 250 1 99 21 22 ) [[ 127 128 129 136 137 138 ]]  rawptr:NotNull ( int:>=0, java/lang/Object:NotNull *, bool, top, bool, bottom, java/lang/Object:NotNull *, long ) allocationKlass:BoxInBoxSmall$BoxOuter BoxInBoxSmall::test @ bci:17 (line 8) !jvms: BoxInBoxSmall::test @ bci:17 (line 8)
+ *
  * Scalar  143  CheckCastPP  === 140 138  [[ 224 ]]  #BoxInBoxSmall$BoxOuter:NotNull:exact *  Oop:BoxInBoxSmall$BoxOuter:NotNull:exact * !jvms: BoxInBoxSmall::test @ bci:17 (line 8)
  * ++++ Eliminated: 126 Allocate
  * Scalar   44  CheckCastPP  === 41 39  [[ 113 ]]  #BoxInBoxSmall$BoxSmall:NotNull:exact *  Oop:BoxInBoxSmall$BoxSmall:NotNull:exact * !jvms: BoxInBoxSmall::test @ bci:7 (line 7)
  * ++++ Eliminated: 27 Allocate
- * AFTER: AFTER_MACRO_EXPANSION
- *    0  Root  === 0 239  [[ 0 1 3 21 22 99 240 ]] inner
- *    1  Con  === 0  [[ ]]  #top
- *    3  Start  === 3 0  [[ 3 5 6 7 8 9 ]]  #{0:control, 1:abIO, 2:memory, 3:rawptr:BotPTR, 4:return_address}
- *    5  Parm  === 3  [[ 239 ]] Control !orig=[32],[41],[114],[131],[140],[225] !jvms: BoxInBoxSmall::test @ bci:-1 (line 5)
- *    6  Parm  === 3  [[ 239 ]] I_O !orig=[38],[137] !jvms: BoxInBoxSmall::test @ bci:-1 (line 5)
- *    7  Parm  === 3  [[ 239 ]] Memory  Memory: @BotPTR *+bot, idx=Bot; !orig=[51] !jvms: BoxInBoxSmall::test @ bci:-1 (line 5)
- *    8  Parm  === 3  [[ 239 ]] FramePtr !jvms: BoxInBoxSmall::test @ bci:-1 (line 5)
- *    9  Parm  === 3  [[ 239 ]] ReturnAdr !jvms: BoxInBoxSmall::test @ bci:-1 (line 5)
- *   21  ConI  === 0  [[ 239 ]]  #int:1234
- *   22  ConI  === 0  [[ 239 ]]  #int:56
- *   99  ConI  === 0  [[ 239 ]]  #int:1
- *  239  Return  === 5 6 7 8 9 returns 240 21 22 99 1  [[ 0 ]]
- *  240  ConL  === 0  [[ 239 ]]  #long:34084862658545
  *
  * --- Compiler Statistics ---
  * Objects scalar replaced = 2, Monitor objects removed = 0, GC barriers removed = 0, Memory barriers removed = 4
