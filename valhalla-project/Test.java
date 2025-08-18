08f644e3cb001b8357d47af36bda0c7b081870ed
Galder Zamarreño <galder@ibm.com>
2025-08-18 11:40:28 +0200

    Update valhalla project makefile and test

--- valhalla-project/Test.java
+++ valhalla-project/Test.java
@@ -1,7 +1,34 @@
 class Test
 {
+    static value class MyValue
+    {
+        int x = 42;
+        // int y = 42;
+    }
+
+    public static void testFill(MyValue[] array)
+    {
+        for (int i = 0; i < array.length; ++i)
+        {
+            array[i] = new MyValue();
+        }
+    }
+
+    public static void testCopy(MyValue[] array1, MyValue[] array2)
+    {
+        for (int i = 0; i < array1.length; ++i)
+        {
+            array1[i] = array2[i];
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        MyValue[] array = new MyValue[100];
+        for (int i = 0; i < 100_000; ++i)
+        {
+            testFill(array);
+            testCopy(array, array);
+        }
     }
 }
