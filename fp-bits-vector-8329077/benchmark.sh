43eb41523d914f4f931ee8369840800f687be477
Galder Zamarreño <galder@ibm.com>
2025-08-25 09:30:57 +0200

    Add dry-run and base-run parameters

--- fp-bits-vector-8329077/benchmark.sh
+++ fp-bits-vector-8329077/benchmark.sh
@@ -3,13 +3,15 @@
 set -ex
 
 CLEAN="false"
+DRY_RUN="false"
+BASE_RUN="false"
 EVENTS="cycles,instructions,branch-misses,br_mis_pred,inst_retired"
 
 if [ "$(uname)" = "Darwin" ]; then
-  ASM_PROFILER="xctraceasm"
-  DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
+    ASM_PROFILER="xctraceasm"
+    DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
 else
-  ASM_PROFILER="perfasm"
+    ASM_PROFILER="perfasm"
 fi
 
 # Check for clean parameter
@@ -23,15 +25,32 @@ if [[ "$1" == "--clean=true" ]]; then
     fi
 fi
 
+# Check for the dry-run parameter
+if [[ "$1" == "--dry-run=true" ]]; then
+    DRY_RUN="true"
+fi
+
+# Check for the base run parameter
+if [[ "$1" == "--base-run=true" ]]; then
+    BASE_RUN="true"
+fi
+
 benchmark_all()
 {
     local branch=$1
     local extra_args=$2
     local rff_prefix=$3
+    local dry_run=$4
 
     local prefix="micro:org\.openjdk\.bench\.vm\.compiler\.TypeVectorOperations.TypeVectorOperationsSuperWord\.convert"
 
-    log TEST=\"${prefix}\.*Bits\.*\" MICRO=\"OPTIONS=-rff ${rff_prefix}.csv ${extra_args}\" CONF=release LOG=warn make test
+    if [[ $DRY_RUN == "true" ]]; then
+        args="${extra_args} -f 1 -i 1 -r 1 -wi 0"
+    else
+        args="${extra_args}"
+    fi
+
+    log TEST=\"${prefix}\.*Bits\.*\" MICRO=\"OPTIONS=-rff ${rff_prefix}.csv ${args}\" CONF=release LOG=warn make test
 }
 
 benchmark_branch()
@@ -58,7 +77,7 @@ log()
 }
 
 if [[ $CLEAN == "true" ]]; then
-  CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
+    CONF=release BUILD_LOG=warn make configure clean-jdk build-jdk
 fi
 
 # Clean .csv files from previous runs
@@ -69,7 +88,9 @@ CONF=release make clean-csv
 
 #benchmark_branch "topic.fp-bits-vector" "-prof ${ASM_PROFILER};FORK=1" "perfasm"
 #benchmark_branch "topic.avoid-cmov.0521.aarch64-x64" "-prof perfnorm:events=${EVENTS}" "perfnorm"
-benchmark_branch "topic.fp-bits-vector" "" "noprof"
+benchmark_branch "topic.fp-bits-vector" "" "noprof" $DRY_RUN
 #benchmark_branch "topic.fp-bits-vector.base" "-prof ${ASM_PROFILER};FORK=1" "perfasm"
 #benchmark_branch "topic.avoid-cmov.0521.base" "-prof perfnorm:events=${EVENTS}" "perfnorm"
-benchmark_branch "topic.fp-bits-vector.base" "" "noprof-base"
+if [[ $BASE_RUN == "true" ]]; then
+    benchmark_branch "topic.fp-bits-vector.base" "" "noprof-base" $DRY_RUN
+fi
