f2cbaf8754659347bb05bbe9e8769160f8d489d0
Galder Zamarreño <galder@ibm.com>
2025-08-28 10:12:05 +0200

    Use jtreg directly consuming variables via the Makefile

--- fp-bits-vector-8329077/ci.sh
+++ fp-bits-vector-8329077/ci.sh
@@ -2,14 +2,19 @@
 
 set -e -x
 
-concat () (
-    IFS=
-    printf '%s' "$*"
-)
+JAVA_HOME=$(make -q print-java-home)
+JDK_HOME=$(make -q print-jdk-home)
+JTREG_BIN=$(make -q print-jtreg-bin)
 
 tests=(
   "test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java"
 )
 
-TEST="${tests[*]}" JTREG="JAVA_OPTIONS=-XX:UseAVX=1" make jtreg
-TEST="${tests[*]}" make jtreg
+make test-image
+
+pushd ${JDK_HOME}
+if [ "$(uname)" = "Linux" ]; then
+    JAVA_HOME=${JAVA_HOME} ${JTREG_BIN} -verbose:all -vmoption:-XX:UseAVX=1 ${tests[*]}
+fi
+JAVA_HOME=${JAVA_HOME} ${JTREG_BIN} -verbose:all ${tests[*]}
+popd
