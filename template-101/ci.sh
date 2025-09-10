02c7af9b6a051b33683e1e4bd1181f7f9746ad9d
Galder Zamarreño <galder@ibm.com>
2025-09-10 11:19:13 +0200

    Add more executions

--- template-101/ci.sh
+++ template-101/ci.sh
@@ -2,4 +2,8 @@
 
 set -e -x
 
-TEST="test/hotspot/jtreg/compiler/valhalla/inlinetypes/templating/TestOne.java" make jtreg
+make clean-jtreg
+
+#TEST="test/hotspot/jtreg/compiler/valhalla/inlinetypes/templating/TestOne.java" make jtreg
+TEST="test/hotspot/jtreg/compiler/valhalla/inlinetypes/templating/ExampleNoNames.java" make jtreg
+TEST="test/hotspot/jtreg/compiler/valhalla/inlinetypes/templating/ExampleStructuralNames.java" make jtreg
