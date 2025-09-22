4e85f59b2cf818694c988117e77ff3328833d991
Galder Zamarreño <galder@ibm.com>
2025-09-22 15:04:54 +0200

    Make it simpler

--- nix-make/uica.sh
+++ nix-make/uica.sh
@@ -2,13 +2,10 @@
 set -eux
 
 DIR=$1
-OPT=$2
 
-pushd ${OPT}/uiCA
 for asmfile in "${DIR}"/*.asm; do
     base="${asmfile%.asm}"   # strip .asm extension
     echo "Assembling $asmfile → ${base}.o"
     as "${asmfile}" -o "${base}.o"
     ./uiCA.py "${base}.o" -arch CLX -trace ${base}.html
 done
-popd
