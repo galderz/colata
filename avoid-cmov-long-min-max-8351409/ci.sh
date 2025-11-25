12468dc92073850bdbebe47ae114e69aa10dea71
Galder Zamarreño <galder@ibm.com>
2025-11-25 16:29:59 +0100

    Adjust for latest tests

--- avoid-cmov-long-min-max-8351409/ci.sh
+++ avoid-cmov-long-min-max-8351409/ci.sh
@@ -7,8 +7,12 @@ if [[ "$1" == "--clean=true" ]]; then
 fi
 
 tests=(
-    "test/hotspot/jtreg/compiler/loopopts/superword/ReductionPerf.java"
-    "test/hotspot/jtreg/compiler/c2/irTests/TestIfMinMax.java"
+    # "test/hotspot/jtreg/compiler/loopopts/superword/ReductionPerf.java"
+    # "test/hotspot/jtreg/compiler/c2/irTests/TestIfMinMax.java"
+    # "test/hotspot/jtreg/compiler/c2/irTests/TestReductionReassociation.java"
+    # "test/hotspot/jtreg/compiler/c2/irTests/TestReductionReassociationManual.java"
+    "test/hotspot/jtreg/compiler/loopopts/TestReductionReassociationFuzzer.java"
+    "test/hotspot/jtreg/compiler/loopopts/TestReductionReassociation.java"
 )
 
 TEST="${tests[*]}" make jtreg
