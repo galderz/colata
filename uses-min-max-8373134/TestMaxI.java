af1756b5095eb4c29f63870d17c7208052fdab88
Galder Zamarreño <galder@ibm.com>
2025-12-09 08:30:29 +0100

    Add failure to max(II)

--- uses-min-max-8373134/TestMaxI.java
+++ uses-min-max-8373134/TestMaxI.java
@@ -8,6 +8,20 @@
  * i is replaced by 1 and a*i is enqueued for igvn,
  * which causes c = a, which causes max(a, b) to be processed
  * but nothing pushes max(a, max(a, b)) for igvn.
+ *
+ * Missed Identity optimization:
+ * Old node:
+ * dist dump
+ * ---------------------------------------------
+ *    1  124  MaxI  === _ 11 10  [[ 136 ]]  !jvms: Integer::max @ bci:2 (line 1934) TestMaxI::test @ bci:21 (line 21)
+ *    1   10  Parm  === 3  [[ 136 124 71 82 38 49 60 ]] Parm0: int !orig=[113] !jvms: TestMaxI::test @ bci:-1 (line 17)
+ *    0  136  MaxI  === _ 10 124  [[ 137 ]]  !jvms: Integer::max @ bci:2 (line 1934) TestMaxI::test @ bci:24 (line 21)
+ * New node:
+ * dist dump
+ * ---------------------------------------------
+ *    1   10  Parm  === 3  [[ 136 124 71 82 38 49 60 ]] Parm0: int !orig=[113] !jvms: TestMaxI::test @ bci:-1 (line 17)
+ *    1   11  Parm  === 3  [[ 124 60 82 71 38 49 ]] Parm1: int !jvms: TestMaxI::test @ bci:-1 (line 17)
+ *    0  124  MaxI  === _ 11 10  [[ 136 ]]  !jvms: Integer::max @ bci:2 (line 1934) TestMaxI::test @ bci:21 (line 21)
  */
 public class TestMaxI
 {
