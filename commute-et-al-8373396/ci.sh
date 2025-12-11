5435d81dab5cdeb1b0606e45bcaa29d616d30869
Galder Zamarreño <galder@ibm.com>
2025-12-11 09:06:33 +0100

    Tested and commented

--- commute-et-al-8373396/ci.sh
+++ commute-et-al-8373396/ci.sh
@@ -7,7 +7,9 @@ if [[ "$1" == "--clean=true" ]]; then
 fi
 
 tests=(
+#    "test/hotspot/jtreg/compiler/codegen/TestBooleanVect.java"
     "test/hotspot/jtreg/compiler/c2/irTests/TestMinMaxIdeal.java"
 )
 
+# TEST="${tests[*]}" JAVA_OPTIONS="-XX:VerifyIterativeGVN=1110" make jtreg jtreg-format
 TEST="${tests[*]}" make jtreg jtreg-format
