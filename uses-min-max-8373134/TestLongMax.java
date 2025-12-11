8f9deea191ef32d0908fb2f3044b0fbd7a02db3a
Galder Zamarreño <galder@ibm.com>
2025-12-11 09:15:15 +0100

    Add Long test case that fails

--- uses-min-max-8373134/TestLongMax.java
+++ uses-min-max-8373134/TestLongMax.java
@@ -11,21 +11,21 @@
  */
 public class TestLongMax
 {
-    private static long test(long a, long b)
+    private static long test(long b, long a)
     {
         int i;
         for (i = -10; i < 1; i++)
         {
         }
         long c = a * i;
-        return Long.max(a, Long.max(b, c));
+        return Long.max(a, Long.max(c, b));
     }
 
     public static void main(String[] args)
     {
         for (int i = 0; i < 20_000; i++)
         {
-            test(42l, 42l);
+            test(42, 42);
         }
     }
 }
