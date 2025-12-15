ff1ff5b82efd759f825d9a8590988a5c222a2625
Galder Zamarreño <galder@ibm.com>
2025-12-15 09:52:58 +0100

    Adjust after refactoring

--- commute-et-al-8373396/ci.sh
+++ commute-et-al-8373396/ci.sh
@@ -8,7 +8,7 @@ fi
 
 tests=(
 #    "test/hotspot/jtreg/compiler/codegen/TestBooleanVect.java"
-    "test/hotspot/jtreg/compiler/c2/irTests/TestMinMaxIdeal.java"
+    "test/hotspot/jtreg/compiler/igvn/TestMinMaxIdeal.java"
 )
 
 # TEST="${tests[*]}" JAVA_OPTIONS="-XX:VerifyIterativeGVN=1110" make jtreg jtreg-format
