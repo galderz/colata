0c14deb89104febc3c7f9729d9f9ac334c5eb919
Galder Zamarreño <galder@ibm.com>
2026-01-28 12:32:07 +0000

    Use a ternary

---
+++ reassoc-reduct-chain-8351409/TestDoubleMaxTernary.java
@@ -0,0 +1,91 @@
+import static java.lang.IO.*;
+import static java.lang.System.nanoTime;
+import static java.util.concurrent.TimeUnit.*;
+
+import java.lang.String;
+import java.util.Random;
+
+final class TestDoubleMaxTernary
+{
+  static final int ITER = 100_000;
+
+  static final int NUM_RUNS = 10;
+
+  static final int SIZE = 10_000;
+
+  static final Random RND = new Random(42);
+
+  static double test(double[] array) {
+    double result = Double.MIN_VALUE;
+    for (int i = 0; i < array.length; i++) {
+      var v = array[i];
+      // result = Math.max(v, result);
+      result = (v >= result) ? v : result;
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    var array = new double[SIZE];
+    init(array);
+    var expected = expect(array);
+    validate(expected, array);
+    println("Warmup");
+    for (int i = 0; i < ITER; i++) {
+      test(array);
+    }
+    println("Running");
+    for (int run = 1; run <= NUM_RUNS; run++) {
+      var t0 = nanoTime();
+      long operations = 0;
+      for (int i = 0; i < ITER; i++) {
+        blackhole(test(array));
+        operations++;
+      }
+      var t1 = nanoTime();
+      var durationNs = t1 - t0;
+      var outputTimeUnit = MILLISECONDS;
+      var throughput = operations * NANOSECONDS.convert(1, outputTimeUnit) / durationNs;
+      println("Throughput: %d ops/ms".formatted(throughput));
+      if (NUM_RUNS == run) {
+        validate(expected, array);
+      }
+    }
+  }
+
+  static void init(double[] array) {
+    for (int i = 0; i < array.length; i++) {
+      array[i] = RND.nextDouble();
+    }
+  }
+
+  static double expect(double[] array) {
+    double result = Double.MIN_VALUE;
+    for (int i = 0; i < array.length; i++) {
+      var v = array[i];
+      var t = Math.max(v, result);
+      result = t;
+    }
+    return result;
+  }
+
+  static void blackhole(double value) {
+    if (value == nanoTime()) {
+      println(value);
+    }
+  }
+
+  static void validate(double expected, double[] array) {
+    println("Validate");
+    var value = test(array);
+    assertEquals(expected, value);
+  }
+
+  static void assertEquals(double expected, double actual) {
+    if(expected == actual) {
+      blackhole(actual);
+    } else {
+      throw new AssertionError("Failed: expected: %d, actual: %d".formatted(expected, actual));
+    }
+  }
+}
