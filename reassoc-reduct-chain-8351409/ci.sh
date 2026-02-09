67c3bf89fc41d6c53d0b55056b948788f994c5c9
Galder Zamarreño <galder@ibm.com>
2026-02-09 12:14:52 +0100

    Update CI and summary

--- reassoc-reduct-chain-8351409/ci.sh
+++ reassoc-reduct-chain-8351409/ci.sh
@@ -9,14 +9,14 @@ fi
 tests=(
     "test/hotspot/jtreg/compiler/loopopts/TestReductionReassociationForAssociativeAdds.java"
     "test/hotspot/jtreg/compiler/gcbarriers/TestMinMaxLongLoopBarrier.java"
-    "test/hotspot/jtreg/compiler/c2/Test5091921.java"
-    "test/hotspot/jtreg/compiler/codecache/TestStressCodeBuffers.java"
-    "test/hotspot/jtreg/compiler/intrinsics/sha/sanity/TestSHA1Intrinsics.java"
-    "test/hotspot/jtreg/compiler/loopopts/superword/ReductionPerf.java"
-    "test/hotspot/jtreg/compiler/vectorapi/VectorMaskLoadStoreTest.java"
-    "test/hotspot/jtreg/compiler/c2/cmove/TestFPComparison2.java"
-    "test/hotspot/jtreg/compiler/loopopts/superword/RedTest_int.java"
-    "test/hotspot/jtreg/compiler/loopopts/superword/RedTest_long.java"
+    # "test/hotspot/jtreg/compiler/c2/Test5091921.java"
+    # "test/hotspot/jtreg/compiler/codecache/TestStressCodeBuffers.java"
+    # "test/hotspot/jtreg/compiler/intrinsics/sha/sanity/TestSHA1Intrinsics.java"
+    # "test/hotspot/jtreg/compiler/loopopts/superword/ReductionPerf.java"
+    # "test/hotspot/jtreg/compiler/vectorapi/VectorMaskLoadStoreTest.java"
+    # "test/hotspot/jtreg/compiler/c2/cmove/TestFPComparison2.java"
+    # "test/hotspot/jtreg/compiler/loopopts/superword/RedTest_int.java"
+    # "test/hotspot/jtreg/compiler/loopopts/superword/RedTest_long.java"
 )
 
 TEST="${tests[*]}" make jtreg
