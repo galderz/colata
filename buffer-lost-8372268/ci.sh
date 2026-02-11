8b194e36f41ca4f024a4fdb7807ee1421902e77b
Galder Zamarreño <galder@ibm.com>
2026-02-11 09:52:24 +0100

    Update ci and summary

--- buffer-lost-8372268/ci.sh
+++ buffer-lost-8372268/ci.sh
@@ -7,7 +7,9 @@ if [[ "$1" == "--clean=true" ]]; then
 fi
 
 tests=(
-    "test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestBufferLost.java"
+    # "test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestBufferLost.java"
+    "test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestArrayMetadata.java#xcomp"
+    "test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestArrays.java#id0"
 )
 
 # Run individual tests first
