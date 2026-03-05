8cb178acdbb81b75ed1b3887c73f3e1986d2c951
Galder Zamarreño <galder@ibm.com>
2026-03-05 17:48:46 +0100

    Remove outdated efforts

--- nix-darwin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -x
-
-#pushd jdk && LOG=debug,cmdlines make
-pushd $(HOME)/1/jdk && make
--- nix-darwin/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -x
-
-pushd $(HOME)/1/jdk && bash configure \
-    --with-boot-jdk=$(dirname $(dirname $(readlink -f $(which java)))) \
-    --with-devkit=$DEVKIT_ROOT
