e52d7ad10a19ac07a232be314eb5e431c0ec7204
Galder Zamarreño <galder@ibm.com>
2026-03-09 06:29:50 +0100

    Simplified test to avoid src MemorySegment

--- segment-ir-tests-8367399/MoveFloatToInt.java
+++ segment-ir-tests-8367399/MoveFloatToInt.java
@@ -1,4 +1,5 @@
 import java.lang.foreign.*;
+import java.util.*;
 
 /**
  * $ CLASS=MoveFloatToInt make PRINT=BEFORE_LOOP_UNROLLING
@@ -19,18 +20,22 @@ class MoveFloatToInt
 {
     static final int ITER = 10_000;
 
-    static void test(MemorySegment src, MemorySegment dst)
+    static void test(float[] src, MemorySegment dst)
     {
         for (int i = 0; i < 2_000; i++)
         {
-            float v = (float) src.get(ValueLayout.JAVA_DOUBLE_UNALIGNED, 8L * i);
-            dst.set(ValueLayout.JAVA_FLOAT_UNALIGNED, 4L * i, v);
+            dst.set(ValueLayout.JAVA_FLOAT_UNALIGNED, 4L * i, src[i]);
         }
     }
 
     public static void main(String[] args)
     {
-        MemorySegment src = MemorySegment.ofArray(new float[4_000]);
+        Random rnd = new Random();
+        float[] src = new float[2_000];
+        for (int i = 0; i < 2_000; i++) {
+            src[i] = rnd.nextFloat();
+        }
+
         MemorySegment dst = MemorySegment.ofArray(new float[2_000]);
         for (long i = 0; i < ITER; i++)
         {
