7e5f85effe225f55b7a18e9c2ffc33e0f5a2200f
Galder Zamarreño <galder@ibm.com>
2026-04-16 07:44:43 +0200

    Update for JDK 26

--- make/linux.nix
+++ make/linux.nix
@@ -17,6 +17,24 @@ let
 
     set debuginfod enabled on
   '';
+
+    temurin26 = pkgs.stdenvNoCC.mkDerivation rec {
+      pname = "temurin-26";
+      version = "26+35";
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/adoptium/temurin26-binaries/releases/download/jdk-26+35/OpenJDK26U-jdk_x64_linux_hotspot_26_35.tar.gz";
+        hash = "sha256-aOGbpTt/H3RjXBP4CeXbNs68zzrp51JCPdktKtfYMe8=";
+      };
+
+      dontConfigure = true;
+      dontBuild = true;
+
+      installPhase = ''
+        mkdir -p $out
+        tar -xzf $src --strip-components=1 -C $out
+      '';
+    };
 in
 pkgs.mkShell {
   hardeningDisable = [ "all" ]; # Disable all hardening
@@ -39,11 +57,11 @@ pkgs.mkShell {
     xorg.libXrandr
     zip
   ] ++ [
-    unstable.temurin-bin-25
+    temurin26
   ];
 
   ANT_HOME="${pkgs.ant}/share/ant";
-  BOOT_JDK_HOME="${unstable.temurin-bin-25}";
+  BOOT_JDK_HOME="${unstable.temurin-26}";
   CAPSTONE_HOME="${pkgs.capstone}";
   FREETYPE_INCLUDE="${pkgs.freetype.dev}/include";
   FREETYPE_LIB="${pkgs.freetype}/lib";
