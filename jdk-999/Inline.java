d04e6de344f1f5a3f3605cb6c873379346fc66c5
Galder Zamarreño <galder@ibm.com>
2026-04-17 09:08:49 +0200

    New example, more help

---
+++ jdk-999/Inline.java
@@ -0,0 +1,28 @@
+public class Inline
+{
+    public static void main(String[] args)
+    {
+        // Especially with a debug build, the JVM startup can take a while,
+        // so it can take a while until our code is executed.
+        System.out.println("Run");
+
+        // Repeatedly call the test method, so that it can become hot and
+        // get JIT compiled.
+        for (int i = 0; i < 10_000; i++)
+        {
+            test(i, i + 1);
+        }
+        System.out.println("Done");
+    }
+
+    // The test method we will focus on.
+    public static int test(int a, int b)
+    {
+        return multiply(101, a) + multiply(202, a) + multiply(53, b);
+    }
+
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
