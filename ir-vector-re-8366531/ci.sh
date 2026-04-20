f70d0e98cdf4828592302fedd8922563065e0a33
Galder Zamarreño <galder@ibm.com>
2026-04-20 16:17:52 +0200

    Add ir-vector-re

---
+++ ir-vector-re-8366531/ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+if [[ "$1" == "--clean=true" ]]; then
+  make clean-jtreg
+fi
+
+tests=(
+    "test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java"
+    "test/hotspot/jtreg/compiler/loopopts/superword/TestReinterpretAndCast.java"
+)
+
+TEST="${tests[*]}" make jtreg
