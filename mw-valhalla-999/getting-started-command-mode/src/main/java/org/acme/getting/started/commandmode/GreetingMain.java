f08a5a4532819b8774e03df48f19dbbfc809aefb
Galder Zamarreño <galder@ibm.com>
2026-04-21 14:26:37 +0200

    Test instantiaing RequestMatch

--- mw-valhalla-999/getting-started-command-mode/src/main/java/org/acme/getting/started/commandmode/GreetingMain.java
+++ mw-valhalla-999/getting-started-command-mode/src/main/java/org/acme/getting/started/commandmode/GreetingMain.java
@@ -4,6 +4,8 @@ import jakarta.inject.Inject;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import org.jboss.resteasy.reactive.server.mapping.RequestMapper;
+import org.jboss.resteasy.reactive.server.mapping.URITemplate;
 
 @QuarkusMain
 public class GreetingMain implements QuarkusApplication {
@@ -19,6 +21,15 @@ public class GreetingMain implements QuarkusApplication {
         } else {
             System.out.println(service.greeting("commando"));
         }
+
+        final RequestMapper.RequestMatch<String> requestMatch = new RequestMapper.RequestMatch<>(
+            new URITemplate("abc", false)
+            , "def"
+            , new String[]{"ghi", "jkl"}
+            , "mno"
+        );
+
+        System.out.println(service.greeting(requestMatch.toString()));
 
         return 0;
     }
