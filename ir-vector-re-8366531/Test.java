e5c8b1766d83675fea6a67ccb51c3fc94d0481a1
Galder Zamarreño <galder@ibm.com>
2026-04-21 10:48:33 +0200

    Add a basic Test target to run

---
+++ ir-vector-re-8366531/Test.java
@@ -0,0 +1,23 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        // Especially with a debug build, the JVM startup can take a while,
+        // so it can take a while until our code is executed.
+        System.out.println("Run");
+
+        // Repeatedly call the test method, so that it can become hot and
+        // get JIT compiled.
+        for (int i = 0; i < 10_000; i++)
+        {
+            test(i, i + 1);
+        }
+        System.out.println("Done");
+    }
+
+    // The test method we will focus on.
+    public static int test(int a, int b)
+    {
+        return a + b;
+    }
+}
