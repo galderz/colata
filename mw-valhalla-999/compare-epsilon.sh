5d310c4ebec3db67df0589d006f9e949717a5cbf
Galder Zamarreño <galder@ibm.com>
2026-05-08 15:38:55 +0200

    Limit to Quarkus 3, run with epsilon gc and get heap dump

--- mw-valhalla-999/compare-epsilon.sh
+++ mw-valhalla-999/compare-epsilon.sh
@@ -21,7 +21,7 @@ cd $REPO/scripts/perf-lab
   --host 127.0.0.1 \
   --iterations 1 \
   --java-version 25.0.2-tem \
-  --jvm-args "-XX:+UseNUMA -Dserver.tomcat.threads.max=50 -Dserver.tomcat.threads.min-spare=50" \
+  --jvm-args "-XX:+UseNUMA -Dserver.tomcat.threads.max=50 -Dserver.tomcat.threads.min-spare=50 -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:+HeapDumpOnOutOfMemoryError" \
   --jvm-memory "-Xmx512m -Xms512m" \
   --repo-branch $BRANCH \
   --repo-url $REPO \
@@ -31,7 +31,7 @@ cd $REPO/scripts/perf-lab
   --output-dir run \
   --profiler none \
   --quarkus-version 3.34.1 \
-  --runtimes quarkus3-jvm,spring4-jvm \
+  --runtimes quarkus3-jvm \
   --run-identifier local-1 \
   --tests run-load-test \
   --user $QDUP_USER \
