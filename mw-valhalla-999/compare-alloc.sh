0526c1d4a5596e666abfc34f19786ff163ea9c9b
Galder Zamarreño <galder@ibm.com>
2026-05-11 10:10:20 +0200

    Add gc and jfr profile to compare allocation rates

--- mw-valhalla-999/compare-alloc.sh
+++ mw-valhalla-999/compare-alloc.sh
@@ -8,6 +8,9 @@ QDUP_USER=$USER
 
 cd $REPO/scripts/perf-lab
 
+GC="-Xlog:gc*:file=gc.log:level,time,tags,uptime"
+PROFILER="jfr"
+
 ./run-benchmarks.sh \
   --cpus-app 28,29,30,31 \
   --cpus-db 24,25,26 \
@@ -21,7 +24,7 @@ cd $REPO/scripts/perf-lab
   --host 127.0.0.1 \
   --iterations 1 \
   --java-version 25.0.2-tem \
-  --jvm-args "-XX:+UseNUMA -Dserver.tomcat.threads.max=50 -Dserver.tomcat.threads.min-spare=50 -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$HOME/tmp/quarkus.hprof" \
+  --jvm-args "-XX:+UseNUMA -Dserver.tomcat.threads.max=50 -Dserver.tomcat.threads.min-spare=50 $GC" \
   --jvm-memory "-Xmx512m -Xms512m" \
   --repo-branch $BRANCH \
   --repo-url $REPO \
@@ -29,7 +32,7 @@ cd $REPO/scripts/perf-lab
   --springboot3-version 3.5.13 \
   --springboot4-version 4.0.5 \
   --output-dir run \
-  --profiler none \
+  --profiler $PROFILER \
   --quarkus-version 3.34.1 \
   --runtimes quarkus3-jvm \
   --run-identifier local-1 \
