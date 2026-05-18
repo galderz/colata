9bed82bde841e7da757ed9815210ddca16d4de7a
Galder Zamarreño <galder@ibm.com>
2026-05-18 10:01:51 +0200

    More fixing of env variable name

--- double-while-loop-8312011/jdk17.nix
+++ double-while-loop-8312011/jdk17.nix
@@ -6,7 +6,7 @@ pkgs.mkShell {
     temurin-bin-17
   ];
 
-  JDK_17_HOME="${pkgs.temurin-bin-17}";
+  JAVA_17_HOME="${pkgs.temurin-bin-17}";
 
   shellHook = ''
     echo "JAVA_17_HOME set to $JAVA_17_HOME"
