fbefdf13e5403ca2749436c4e5ea9863d0f85377
Galder Zamarreño <galder@ibm.com>
2026-05-27 13:53:13 +0200

    Add script for aws

---
+++ ir-vector-re-8366531/aws.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+sudo apt-get install -y \
+  autoconf \
+  build-essential \
+  make
+
+popd src
+git clone https://github.com/galderz/jdk
+
+popd jdk
+git worktree add \
+  -b topic.ir-vector-re.expand-use \
+  ../jdk-ir-vector-re \
+  origin/topic.ir-vector-re.expand-use
+popd
+
+popd colata/ir-vector-re-8366531
+
+BOOT_JDK_VERSION=25 make boot-jdk
+JAVA_25_HOME=$HOME/opt/boot-java-25
+
+BOOT_JDK_VERSION=26 make boot-jdk
+JAVA_26_HOME=$HOME/opt/boot-java-26
+
+NO_HSDIS=true make configure build-jdk
+
+TEST="test/hotspot/jtreg/compiler/vectorapi/reshape/TestVectorReinterpret.java" make clean-jtreg jtreg
