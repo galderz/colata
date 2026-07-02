ac6b89307c55ea264866aece5bc9694bdc9dae53
Galder Zamarreño <galder@ibm.com>
2026-07-02 14:21:06 +0100

    Update test executed by ci

--- clone-conv-999/ci.sh
+++ clone-conv-999/ci.sh
@@ -6,8 +6,10 @@ if [[ "$1" == "--clean=true" ]]; then
   make clean-jtreg
 fi
 
+# "test/hotspot/jtreg/compiler/loopopts/superword/TestReductions.java"
+# "test/hotspot/jtreg/compiler/c2/TestByteArrayAddressing.java"
 tests=(
-    "test/hotspot/jtreg/compiler/c2/TestByteArrayAddressing.java"
+    "test/hotspot/jtreg/compiler/c2/TestArrayAddressing.java"
 )
 
 TEST="${tests[*]}" make jtreg
