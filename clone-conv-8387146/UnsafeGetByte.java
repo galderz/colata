eea6e9468b47210c667c3a63a4a43962a91efd91
Galder Zamarreño <galder@ibm.com>
2026-08-25 15:20:56 +0200

    Add reproducer for unsafe get byte regression

---
+++ clone-conv-8387146/UnsafeGetByte.java
@@ -0,0 +1,31 @@
+public class UnsafeGetByte
+{
+    public static void main(String[] args)
+    {
+        // Especially with a debug build, the JVM startup can take a while,
+        // so it can take a while until our code is executed.
+        System.out.println("Run");
+
+        byte[] a = new byte[1024];
+        byte[] b = new byte[1024];
+        a[42] = 20;
+        b[42] = 22;
+
+        int offset = BASE + 42;
+
+        // Repeatedly call the test method, so that it can become hot and
+        // get JIT compiled.
+        for (int i = 0; i < 10_000; i++)
+        {
+            test(a, b, offset);
+        }
+        System.out.println("Done");
+    }
+
+    // The test method we will focus on.
+    public static int test(byte[] a, byte[] b, int offset)
+    {
+        int checkedOffset = Objects.checkIndex(offset, 1000);
+        return UNSAFE.getByte(a, (long) checkedOffset) + UNSAFE.getByte(b, (long) checkedOffset);
+    }
+}
