ba5fa0eb9cb76d0a4538a6cbd6e0b9c74bac2e3c
Galder Zamarreño <galder@ibm.com>
2026-08-27 06:48:01 +0000

    Add DiffOffset reproducer

---
+++ clone-conv-8387146/DiffOffset.java
@@ -0,0 +1,26 @@
+public class DiffOffset
+{
+        public static void main(String[] args)
+    {
+        // Especially with a debug build, the JVM startup can take a while,
+        // so it can take a while until our code is executed.
+        System.out.println("Run");
+
+        byte[] b = new byte[1024];
+	int index = 42;
+        b[index] = 22;
+
+        // Repeatedly call the test method, so that it can become hot and
+        // get JIT compiled.
+        for (int i = 0; i < 10_000; i++)
+        {
+            test(b, index);
+        }
+        System.out.println("Done");
+    }
+
+    static int test(byte[] b, int i) {
+        i = Integer.min(Integer.max(i, 0), 1000);
+        return b[i] + b[i + 1];
+    }
+}
