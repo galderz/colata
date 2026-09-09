b8e811c67126c607d0d7774e265a88508d39db91
Galder Zamarreño <galder@ibm.com>
2026-09-09 09:40:15 +0200

    Add reproducer

---
+++ ap-vc-1786/run_reproducer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Compile
+$JAVA_HOME/bin/javac --source 28 AllocReproducer.java
+
+# Run
+$JAVA_HOME/bin/java --enable-preview -agentpath:$HOME/src/async-profiler/build/lib/libasyncProfiler.so=start,event=alloc,file=target/alloc_profile.jfr AllocReproducer
+
+# Check the recording
+jfr print --events jdk.ObjectAllocationInNewTLAB target/alloc_profile.jfr | grep 'objectClass = ' | sed 's/.*objectClass = //' | sort | uniq -c | sort -rn
